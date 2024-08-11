7f406528159f9af6952ccfeb1daefe5513a785f6
lyxyyds123456 <142554809+lyxyyds123456@users.noreply.github.com>
2024-08-12 01:07:24 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -29,16 +29,16 @@ const USER_CONFIG = {
       horoscopeDate: '12-27',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
+        //{
+        // type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+        //},
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '2005', date: '11-16',
+          type: '生日', name: '姜大美女', year: '2005', date: '11-16',
         },
-        {
-          type: '节日', name: '相识纪念日', year: '2024', date: '07-01',
-        },
+        //{
+        //  type: '节日', name: '', year: '2024', date: '07-01',
+        //},
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
