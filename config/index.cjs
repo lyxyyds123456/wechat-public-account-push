fd63b2c23f84bbed4a326e28cb44fd60e4d662de
lyxyyds123456 <142554809+lyxyyds123456@users.noreply.github.com>
2024-08-12 11:53:02 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -22,7 +22,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '姜念',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PDU31534T2NU7el0v4t11hPMkKzuWIRbHH46khd6p',
+      id: 'PDU31536TW0hHiNH0nFOLanyPiWeRCffnS33KTgeo',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
