a9a099fff16866c13a46e579e7067a558217a5a7
lyxyyds123456 <142554809+lyxyyds123456@users.noreply.github.com>
2024-08-12 08:23:23 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,7 +6,7 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
-  USE_PASSAGE: 'push-plus',
+  USE_PASSAGE: 'push-deer',
 
   // 使用微信测试号：公众号APP_ID
   APP_ID: 'wx149a0c652eb26377',
@@ -22,7 +22,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '姜念',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '22ce301ba1844350aa1aaccdb4d62e40',
+      id: 'PDU31534T2NU7el0v4t11hPMkKzuWIRbHH46khd6p',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -58,7 +58,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '3aed06996f944ea99747a6bd29acc98d',
+      id: 'PDU31534T2NU7el0v4t11hPMkKzuWIRbHH46khd6p',
     }
   ],
 
